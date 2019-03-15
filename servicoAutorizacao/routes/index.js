2f7da0b69789853664c6856078f8e1a101cc08ca
nall1994 <34685054+nall1994@users.noreply.github.com>
2019-03-15 10:56:00 +0000

    serviços estão feitos.À espera de alteração nas rotas internas apenas após criação dos containers

--- servicoAutorizacao/routes/index.js
+++ servicoAutorizacao/routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+Os pedidos internos como o /checkToken e o /needAuth devem comunicar diretamente com o container
+posteriormente e não pelo localhost.
+Basicamente o localhost só deve escutar pedidos externos. Tudo o que seja troca de informação entre os dois
+deve ser feita utilizando os nomes dos containers pela rede interna.
+*/
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // apresentar página de login ou registo
   res.render('index');
 });
 
-router.get('/checkToken',(req,res) => {
-  token_received = req.query.token
+router.get('/needAuth', (req,res) => {
+  res.render('index',{info_err: 'Autentique-se para obter um token de acesso para utilizar o serviço de email!'})
 })
 
 module.exports = router;
--- servicoEmail/routes/index.js
+++ servicoEmail/routes/index.js
@@ -2,10 +2,34 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios')
 
+/*
+Os pedidos internos como o /checkToken e o /needAuth devem comunicar diretamente com o container
+posteriormente e não pelo localhost.
+Basicamente o localhost só deve escutar pedidos externos. Tudo o que seja troca de informação entre os dois
+deve ser feita utilizando os nomes dos containers pela rede interna.
+*/
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // Verificar se o utilizador tem um token válido!
-  if(req.cookies.auth) {
+  if(req.query.access_token) {
+    access_token = req.query.access_token
+    axiosConfig = {
+      params: req.query
+    }
+    axios.get('http://localhost:3001/users/checkToken',axiosConfig)
+      .then(dados => {
+        dados = dados.data
+        if(dados.message == 'OK') {
+          var user = dados.user
+          res.render('index')
+        } else {
+          // need authentication
+          res.redirect('http://localhost:3001/needAuth')
+        }
+      })
+      .catch(error => res.render('error',{error:error, message: 'Ocorreu um erro ao verificar o token!'}))
+  } else if(req.cookies.auth) {
     access_token = req.cookies.auth
     req.query.access_token = access_token
     axiosConfig = {
@@ -18,13 +42,14 @@ router.get('/', function(req, res, next) {
           var user = dados.user
           res.render('index');
         } else {
-          res.render('auth')
-          //token não existe ou não é válido, fazer outra coisa
+          //need authentication
+          res.redirect('http://localhost:3001/needAuth')
         }
       })
       .catch(error => res.render('error',{error: error, message: 'Ocorreu um erro ao verificar o token!'}))
   } else {
-    res.render('auth')
+    //need authentication
+    res.redirect('http://localhost:3001/needAuth')
   }
    
 });
