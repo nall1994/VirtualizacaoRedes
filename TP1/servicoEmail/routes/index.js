55c4e8b59a732790aa1c8ba78029e96d27641a70
nall1994 <34685054+nall1994@users.noreply.github.com>
2019-03-22 19:27:14 +0000

    update docker-compose.index.js

--- TP1/servicoEmail/routes/index.js
+++ TP1/servicoEmail/routes/index.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
     axiosConfig = {
       params: req.query
     }
-    axios.get('http://localhost:3001/users/checkToken',axiosConfig)
+    axios.get('http://servicoauth:4001/users/checkToken',axiosConfig)
       .then(dados => {
         dados = dados.data
         if(dados.message == 'OK') {
@@ -37,7 +37,7 @@ router.get('/', function(req, res, next) {
     axiosConfig = {
       params: req.query
     }
-    axios.get('http://localhost:3001/users/checkToken',axiosConfig)
+    axios.get('http://servicoauth:4001/users/checkToken',axiosConfig)
       .then(dados => {
         dados = dados.data
         if(dados.message == 'OK') {
