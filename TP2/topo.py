7399c147c3330099b42b7b0a42e6bceb4da1232d
nall1994 <34685054+nall1994@users.noreply.github.com>
2019-04-12 10:41:33 +0100

    add topologia

---
+++ TP2/topo.py
@@ -0,0 +1,18 @@
+from mininet.topo import Topo
+
+class MyTopo(Topo):
+
+    def __init(self):
+
+        host1 = self.addHost('h1')
+        host2 = self.addHost('h2')
+        host3 = self.addHost('h3')
+        switch1 = self.addSwitch('s1')
+        switch2 = self.addSwitch('s2')
+
+        self.addLink(host1,switch1)
+        self.addLink(switch1,switch2)
+        self.addLink(switch2,host2)
+        self.addLink(switch2,host3)
+
+topos = { 'mytopo': (lambda : MyTopo())}
