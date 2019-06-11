588008628828b092b75f92fe79cec59c0626e5cf
nall1994 <34685054+nall1994@users.noreply.github.com>
2019-06-11 18:06:17 +0100

    Inserção de ips unicast e endereços mac.

--- TP3/topo.py
+++ TP3/topo.py
@@ -5,6 +5,7 @@ class MyTopo(topo):
     def build(self):
 
         # Numeração dos switches vai de cima para baixo, da esquerda para a direita.
+        # Switches
         switch1 = self.addSwitch('s1')
         switch2 = self.addSwitch('s2')
         switch3 = self.addSwitch('s3')
@@ -14,11 +15,16 @@ class MyTopo(topo):
         switch7 = self.addSwitch('s7')
         switch8 = self.addSwitch('s8')
         switch9 = self.addSwitch('s9')
-        file_server1 = self.addHost('fileServer1')
-        file_server2 = self.addHost('fileServer2')
-        dns_server1 = self.addHost('dnsServer1')
-        dns_server2 = self.addHost('dnsServer2')
 
+        #Hosts (clientes, file servers e dns servers)
+        file_server1 = self.addHost('fileServer1',ip='10.0.0.10', mac='00:00:00:00:00:10')
+        file_server2 = self.addHost('fileServer2',ip='10.0.0.11', mac='00:00:00:00:00:11')
+        dns_server1 = self.addHost('dnsServer1',ip='10.0.0.20', mac='00:00:00:00:00:20')
+        dns_server2 = self.addHost('dnsServer2',ip='10.0.0.21', mac='00:00:00:00:00:21')
+        client1 = self.addHost('client1',ip='10.0.0.30', mac='00:00:00:00:00:30')
+        client2 = self.addHost('client2',ip='10.0.0.31', mac='00:00:00:00:00:31')
+
+        #Links 
         self.addLink(file_server1, switch1)
         self.addLink(file_server2,switch1)
         self.addLink(switch1,switch2)
@@ -34,5 +40,7 @@ class MyTopo(topo):
         self.addLink(switch7,switch8)
         self.addLink(switch7,dns_server2)
         self.addLink(switch8,switch9)
+        self.addLink(client1,switch2)
+        self.addLink(client2,switch9)
 
 topos = { 'mytopo': (lambda : MyTopo())}
